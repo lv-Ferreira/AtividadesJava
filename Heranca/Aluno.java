8f10d53ff8254fe92bf089d04b227f452fc9b579
lv-Ferreira <lvferreira022@gmail.com>
2020-08-21 01:27:41 -0300

    Exercícios de Herança, Encapsulamento e Associação de Classes

---
+++ Heranca/Aluno.java
@@ -0,0 +1,32 @@
+package Heranca;
+
+public class Aluno {
+    private String registroAcademico;
+    private String curso;
+    private float mensalidade;
+
+    public String getRegistroAcademico() {
+        return registroAcademico;
+    }
+
+    public void setRegistroAcademico(String registroAcademico) {
+        this.registroAcademico = registroAcademico;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public float getMensalidade() {
+        return mensalidade;
+    }
+
+    public void setMensalidade(float mensalidade) {
+        this.mensalidade = mensalidade;
+    }
+    
+}
